d0e3295fa7bbb5a3656ee6fba94fcf73f6843314
Ramaaa9461 <rami.guzmi@gmail.com>
2024-06-01 18:45:10 -0300

    Create Sever program

---
+++ Multiplayer/Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
